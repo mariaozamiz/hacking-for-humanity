423997fe20f52ca7b2889747e31151c48d4d10c5
BbeatrizSI <bbeatriz@gmail.com>
2020-11-25 19:07:41 +0100

    added text-shadow and transition in hover

--- src/components/challengesList/ChallengesList.js
+++ src/components/challengesList/ChallengesList.js
@@ -1,19 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import ChallengeCard from '../challengeCard/ChallengeCard';
 import './challengesList.scss';
 
 function ChallengesList({ challenges }) {
     const challengesInfo = challenges.map((challenge) => {
         return (
-            <li key={challenge.id} className={`challenge-card ${challenge.type}`}>
-                <ChallengeCard
-                    id={challenge.id}
-                    type={challenge.type}
-                    image={challenge.image}
-                    name={challenge.name}
-                    description={challenge.description}
-                />
-            </li>
+            <Link
+                to={`/challenge/${challenge.id}`}
+                style={{ textDecoration: 'none' }}
+            >
+                <li key={challenge.id} className={`challenge-card ${challenge.type}`}>
+                     <ChallengeCard
+                        id={challenge.id}
+                        type={challenge.type}
+                        image={challenge.image}
+                        name={challenge.name}
+                        description={challenge.description}
+                    />
+                </li>
+            </Link>
         );
     });
     return <ul className="challenge-list">{challengesInfo}</ul>;
