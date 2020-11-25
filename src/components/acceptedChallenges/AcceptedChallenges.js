492b5cbe4aef906028974397bec5d5114a4d3128
Bárbara B. Sacristán <bbeatriz@gmail.com>
2020-11-25 21:39:58 +0100

    starting logic for accepted challenges

---
+++ src/components/acceptedChallenges/AcceptedChallenges.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './acceptedChallenges.scss';
+
+const AcceptedChallenges = () => {
+    return (
+        <div>
+            AcceptedChallenges
+        </div>
+    );
+};
+
+export default AcceptedChallenges;
