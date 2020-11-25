4052b566c3c1c56111841e59ce213e09b836bf73
Maria Ozamiz <ozamiz.m@gmail.com>
2020-11-25 15:58:34 +0100

    set basic detail card

--- src/components/challengeDetail/ChallengeDetail.js
+++ src/components/challengeDetail/ChallengeDetail.js
@@ -2,37 +2,31 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './challengeDetail.scss';
 
-function ChallengeDetail(props) {
+function ChallengeDetail() {
     return (
         <div className="modal">
-            <div className="modal__dialog">
-                <div className="modal__button">
-                    <Link to="/">
-                        <div x></div>
-                    </Link>
+            <Link className="button__wrapp" to="/">
+                <button className="button__close">X</button>
+            </Link>
+            <div className="modal__content wrapper">
+                <div className="modal__content-image">
+                    <img
+                        className="image"
+                        src="https://cocomaterial.com/media/lie_man_chair.svg"
+                        alt=""
+                        // src={challenge.image}
+                        // alt={`${challenge.name}´s pic`}
+                    ></img>
                 </div>
-                <div className="modal__content">
-                    <div className="modal__content-image">
-                        <img
-                            className="image"
-                            src={props.challenge.image}
-                            alt={`${props.challenge.name}´s pic`}
-                        ></img>
-                    </div>
-                    <div className="modal__content-info">
-                        <ul className="modal__content-list">
-                            <li className="modal__list-name">
-                                {props.challenge.name}
-                            </li>
-                            <li>{`Status: ${props.challenge.status}`}</li>
-                            <li>{`Species: ${props.challenge.species}`}</li>
-                            <li>{`Origin: ${props.challenge.origin.name}`}</li>
-                            <li>{`Episodes: ${props.challenge.episodes.length}`}</li>
-                        </ul>
-                    </div>
+                <div className="modal__content-info">
+                    <h1 className="modal__title">Medita</h1>
+                    <p className="modal__description">
+                        Tómate 10 minutos para meditar y sentir agradecimiento
+                        por todo lo que eres y tienes.
+                    </p>
+                    <span className="modal__type">Autocuidado</span>
                 </div>
             </div>
-            <div className="modal__overlay"></div>
         </div>
     );
 }
